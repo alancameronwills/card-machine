603ac1f03e47e70e73462ea5cf0f12a623f56064
Alan Cameron Wills <alan@cameronwills.org>
2023-06-27 00:04:45 +0100

    fetch code

--- fetch-code.sh
+++ fetch-code.sh
@@ -1,4 +1,12 @@
-(cd ~/src/card-machine
-git fetch --all &&
-git reset --hard origin/master ) &&
+if test ! -d ~/src/card-machine ;
+then 
+	mkdir ~/src
+	cd ~/src
+	git clone https://github.com/alancameronwills/card-machine.git
+fi
+
+
+(  cd ~/src/card-machine
+   git fetch --all &&
+   git reset --hard origin/master ) &&
 cp -ru ~/src/card-machine ~/card-machine
