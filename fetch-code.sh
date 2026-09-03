b7919e180d8ec8fd17c22e822398966f3b909a67
Alan Cameron Wills <alan@cameronwills.org>
2026-09-03 13:42:14 +0100

    record src version

--- fetch-code.sh
+++ fetch-code.sh
@@ -8,8 +8,9 @@
 		cd ~/src
 		git clone ${git:-https://github.com/alancameronwills/card-machine.git}
 	fi
-	
+
 	cd ~/src/card-machine
 	git fetch -q --all &&
-	git reset --hard origin/master 
+	git reset --hard origin/master > log-src-version.log
+	cat log-src-version.log
 } >> log-update.log 2>&1
